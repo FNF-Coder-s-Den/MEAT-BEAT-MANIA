d606805b1b8b10ff7ed476aa930ff3504e18fbde
Alexg1247 <alexglawischnig@gmail.com>
2023-02-18 21:30:22 -0600

    meat beat my ass

---
+++ scripts/menu/credits menu.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var credits = [
+	"alex",
+	"void",
+	"sword"
+]
+
+var cur_select = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_left_pressed():
+	pass # Replace with function body.
